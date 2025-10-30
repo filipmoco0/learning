75359c658ba66a3bc0c8b1894e93b9efe15f9e28
filipmoco0 <113348795+filipmoco0@users.noreply.github.com>
2025-10-30 20:39:45 +0100

    Add do-while loop for number display and counting
    Implemented a do-while loop to print numbers from 6 to 48 and count them.

---
+++ C++/loops/do-while.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int br = 0;
+    int i = 6;
+
+    do {
+        cout << i << endl;
+        br++;
+        i += 6;
+
+    } while (i <= 48);
+    cout << "There are " << br << " numbers." << endl;
+}
